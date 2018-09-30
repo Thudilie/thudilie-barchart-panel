a526e444226fa4db4cde4fab888dada8d52c1eed
Thuy-Anh Nguyen <s59292@beuth-hochschule.de>
2018-09-30 20:22:45 +0200

    C2101-added Timeseries from grafana

--- src/barchart_ctrl.js
+++ src/barchart_ctrl.js
@@ -1,10 +1,32 @@
-import {PanelCtrl} from 'app/plugins/sdk';
+import {MetricsPanelCtrl} from 'app/plugins/sdk';
+import TimeSeries from 'app/core/time_series';
 import './css/clock-panel.css!';
 
-export class BarchartCtrl extends PanelCtrl {
+export class BarchartCtrl extends MetricsPanelCtrl {
   constructor($scope, $injector) {
     super($scope, $injector);
+    this.events.on('data-received', this.onDataReceived.bind(this));
   }
+
+  onDataReceived(data) {
+    console.log('this is my Data');
+    console.log(data);
+    this.series = data.map(this.seriesHandler.bind(this));
+    console.log('this is my formatted Data');
+    console.log(this.series);
+  }
+
+  seriesHandler(seriesData) {
+    // TimeSeries is needed to get the stats of Grafana
+    const series = new TimeSeries({
+      datapoints: seriesData.datapoints,
+      alias: seriesData.target
+    });
+
+    series.flotpairs = series.getFlotPairs(this.panel.nullPointMode);
+    return series;
+  }
+
 
   get message() {
     return 'HHHHHHEEEE';

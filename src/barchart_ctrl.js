61e8dffb14162e91aea1790d650f428c7b02efc7
Thuy-Anh Nguyen <s59292@beuth-hochschule.de>
2018-10-01 21:35:01 +0200

    C2101-panel updates on rendering/newdata

--- src/barchart_ctrl.js
+++ src/barchart_ctrl.js
@@ -1,11 +1,20 @@
 import {MetricsPanelCtrl} from 'app/plugins/sdk';
 import TimeSeries from 'app/core/time_series';
+import 'jquery';
 import './css/clock-panel.css!';
 
 export class BarchartCtrl extends MetricsPanelCtrl {
   constructor($scope, $injector) {
     super($scope, $injector);
+    this.index = 0;
     this.events.on('data-received', this.onDataReceived.bind(this));
+    this.events.on('render', this.onRender.bind(this));
+  }
+
+  onRender() {
+    const graph = this.elem.find('.barchart-graph')[0];
+    this.index += 1;
+    graph.innerHTML = 'Hello <b>world</b>!' + this.index;
   }
 
   onDataReceived(data) {
@@ -13,7 +22,8 @@ export class BarchartCtrl extends MetricsPanelCtrl {
     console.log(data);
     this.series = data.map(this.seriesHandler.bind(this));
     console.log('this is my formatted Data');
-    console.log(this.series);
+    console.log(this.series[0].stats);
+    this.render();
   }
 
   seriesHandler(seriesData) {
@@ -22,14 +32,16 @@ export class BarchartCtrl extends MetricsPanelCtrl {
       datapoints: seriesData.datapoints,
       alias: seriesData.target
     });
-
     series.flotpairs = series.getFlotPairs(this.panel.nullPointMode);
     return series;
   }
 
-
   get message() {
-    return 'HHHHHHEEEE';
+    return JSON.stringify(this.series);
+  }
+
+  link(scope, elem, attrs, ctrl) {
+    this.elem = elem;
   }
 }
 

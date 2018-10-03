6e8bbf37950f42ff1bfdc02afaad7076d2ec927f
Thuy-Anh Nguyen <s59292@beuth-hochschule.de>
2018-10-03 02:31:41 +0200

    C2101-loopthrough dummydata with D3.js

--- src/barchart_ctrl.js
+++ src/barchart_ctrl.js
@@ -1,23 +1,21 @@
 import {MetricsPanelCtrl} from 'app/plugins/sdk';
 import TimeSeries from 'app/core/time_series';
-import * as d3 from "d3";
 import rendering from './rendering';
 import './css/clock-panel.css!';
 
 export class BarchartCtrl extends MetricsPanelCtrl {
   constructor($scope, $injector) {
     super($scope, $injector);
-    this.index = 0;
     this.events.on('data-received', this.onDataReceived.bind(this));
-    this.events.on('render', this.onRender.bind(this));
+    // this will happen always wenn the chart is rendering unattached from data-received
+    // this.events.on('render', this.onRender.bind(this));
   }
-
+  /*
   onRender() {
     console.log('einmalRendern');
-  }
+  }*/
 
   onDataReceived(data) {
-    d3.select('.test').text('D3 start');
     console.log('this is my Data');
     console.log(data);
     this.series = data.map(this.seriesHandler.bind(this));

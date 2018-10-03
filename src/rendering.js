7840f1767a716978e684f0ea6f2e3908e595b778
Thuy-Anh Nguyen <s59292@beuth-hochschule.de>
2018-10-03 21:52:17 +0200

    C2101-added vertically scaling to bar

--- src/rendering.js
+++ src/rendering.js
@@ -9,22 +9,28 @@ export default function link(scope, elem, attrs, ctrl) {
   let index = 0;
   const graph = elem.find('.barchart-graph')[0];
   const myData = [20, 90];
-  const baressentials = {'height': 20, 'offset': 5};
 
   function createSVG() {
     const chartwidth = parseInt(d3.select('#chart').style('width'), 10);
+    const chartheight = parseInt(d3.select('#chart').style('height'), 10);
+
     // this d3method scales the data and makes the chart responsive
     var xScale = d3.scaleLinear()
       .domain([10, d3.max(myData)]) // original information // max number of sequence
       .range([0, chartwidth]); // where we wanne fit it into
+
+    var yScale = d3.scaleBand()
+      .domain(d3.range(0, myData.length))
+      .range([0, chartheight])
+      .padding(0.2);
 
     // select chart from html and append a svg
     d3.select('#chart').append('svg').classed('barchart', 'true') // this svg will have the class barchart
     .selectAll('rect').data(myData).enter() // select all what is not created go through data
     .append('rect')
     .attr('width', function (data) { return xScale(data);})
-    .attr('height', function () { return baressentials.height;})
-    .attr('y', function (data, ind) { return ind * (baressentials.offset + baressentials.height);})
+    .attr('height', function () { return yScale.bandwidth();})
+    .attr('y', function (data, ind) { return yScale(ind);})
     .classed('bar', 'true');
   }
 

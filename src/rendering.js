4c5f0148f68b13d92a8e9b2823b0e02202f669fe
Thuy-Anh Nguyen <s59292@beuth-hochschule.de>
2018-10-03 02:31:41 +0200

    C2101-loopthrough dummydata with D3.js

--- src/rendering.js
+++ src/rendering.js
@@ -1,3 +1,5 @@
+import * as d3 from 'd3';
+
 export default function link(scope, elem, attrs, ctrl) {
   // if ctrl renders, the renderfunction of this export will be called
   ctrl.events.on('render', function () {
@@ -6,8 +8,24 @@ export default function link(scope, elem, attrs, ctrl) {
 
   let index = 0;
   const graph = elem.find('.barchart-graph')[0];
+  const myData = [{ 'width': '20px', 'height': '90px'}, {'width': '90px', 'height': '20px'}];
+
+  function createSVG() {
+    d3.select('#chart').append('svg').classed('barchart', 'true')
+    .selectAll('rect').data(myData).enter()
+    .append('rect')
+    .attr('width', function (data) {
+      return data.width;
+    })
+    .attr('height', function (data) {
+      return data.height;
+    })
+    .classed('bar', 'true');
+  }
 
   function render() {
+    d3.select('svg').remove();
+    createSVG();
     index += 1;
     graph.innerHTML = 'Hello <b>world</b>!' + index;
   }
